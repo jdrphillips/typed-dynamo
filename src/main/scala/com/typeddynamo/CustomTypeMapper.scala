ec9b888b532fb33bf0bf557132e0dd163930ff70
jdrphillips <jdr.phillips@cantab.net>
2016-05-08 09:48:31 +0100

    Introduce CustomTypeMapper
    So we can support our own custom types in the code,
    and have them automatically serialized to and from dynamo-
    supported types

---
+++ src/main/scala/com/typeddynamo/CustomTypeMapper.scala
@@ -0,0 +1,28 @@
+package com.typeddynamo
+
+trait CustomTypeMapper[T] extends TypeMapper[T] {
+  type Primitive
+  implicit def primitiveEvidence: DynamoPrimitiveTypeMapper[Primitive]
+
+  def _serialize(t: T): Primitive
+  def _deserialize(u: Primitive): T
+
+  def serialize(t: T): DynamoValue[Any] = primitiveEvidence.serialize(_serialize(t))
+  def deserialize(u: DynamoValue[Any]): T = _deserialize(primitiveEvidence.deserialize(u))
+}
+
+object CustomTypeMapper {
+
+  def apply[T, U](
+    to: T => U,
+    from: U => T
+  )(implicit primitive: DynamoPrimitiveTypeMapper[U]): CustomTypeMapper[T] = {
+    new CustomTypeMapper[T] {
+      type Primitive = U
+      implicit def primitiveEvidence = primitive
+      def _serialize(t: T): Primitive = to(t)
+      def _deserialize(u: Primitive): T = from(u)
+    }
+  }
+
+}
