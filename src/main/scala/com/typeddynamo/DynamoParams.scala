1f003a8ee22de7ba7fed9a133407ee0bb5d9650f
jdrphillips <jdr.phillips@cantab.net>
2016-05-08 10:28:06 +0100

    Fix error message

--- src/main/scala/com/typeddynamo/DynamoParams.scala
+++ src/main/scala/com/typeddynamo/DynamoParams.scala
@@ -5,7 +5,8 @@ case class DynamoParams(params: Map[String, Option[DynamoValue[Any]]]) {
   private def get[T <: DynamoEntity, U](column: Column[U])(implicit table: DynamoTable[T]): Option[DynamoValue[Any]] = {
     val x: Option[DynamoValue[Any]] = params
       .get(column.name)
-      .getOrElse(throw new Exception(s"No value for column ${column.name} was found in dynamo for table $table"))
+      // TODO: Will this cause a problem? Requires testing.
+      .getOrElse(throw new Exception(s"No value for column ${column.name} was found in dynamo for table ${table.name}"))
     x
   }
 
@@ -18,7 +19,7 @@ case class DynamoParams(params: Map[String, Option[DynamoValue[Any]]]) {
 
   def notNull[T <: DynamoEntity, U](column: Column[U])(implicit table: DynamoTable[T], mapper: TypeMapper[U]): U = {
     val x = get[T, U](column)
-    val any = x.getOrElse(throw new Exception(s"Null value found in column ${column.name} for table $table"))
+    val any = x.getOrElse(throw new Exception(s"Null value found in column ${column.name} for table ${table.name}"))
     mapper.deserialize(any)
   }
 
